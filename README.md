ee274590e4286a191e058670595a075f565af56e
Pete Vander Giessen <pete.vandergiessen@canonical.com>
2017-06-12 16:18:48 -0400

    Laid out repo.
    Stubbed out important files. Began on a setup.py and README.md.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # snapstack
-Integration Testing Harness for Openstack Snaps
+Integration Testing Harness for Openstack Snaps [Prototype]
+
+This is a first pass at an Integration Testing Harness for Openstack Snaps, intended for use in the Canonical Openstack Team's CI system.
+
+Usually, this testing harness would be imported into a test in the same repo as the snapcraft.yaml for an individual test. A developer would then be able to run the tests with tox.
---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+reqs_file = os.path.join(this_dir, 'requirements.txt')
+with open(reqs_file) as f:
+    reqs = [line for line in f.read().splitlines()
+            if not line.startswith('--')]
+
+SETUP = {
+    'name': "snapstack",
+    'packages': find_packages(),
+    'version': "0.0.1",
+    'author': "Pete Vander Giessen",
+    'author_email': "petevg@canonical.com",
+    'description': "Openstack Snap CI Harness [Prototype]"
+    'url': "https://github.com/petevg/snapstack",
+    'license': "Apache 2 License",
+    'long_description': open('README.md').read(),
+    'install_requires': reqs,
+}
+
+
+if __name__ == '__main__':
+    setup(**SETUP)
