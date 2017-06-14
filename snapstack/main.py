929ccdda8ed92340169dfc90ec6cfa9afc313738
Pete Vander Giessen <pete.vandergiessen@canonical.com>
2017-06-14 10:59:29 -0400

    First pass at test runner.
    Added Runner class to main. It does all the things that I believe that
    it need to do.
    
    Github fetching stuff is still stubbed out, however.

---
+++ snapstack/main.py
@@ -0,0 +1,111 @@
+import os
+
+import subprocess
+import unittest
+
+class InfraFailure(Exception):
+    pass
+
+class TestFailure(Exception):
+    pass
+
+
+class Runner:
+    '''
+    The heart of snapstack: tools to build an openstack environemnt
+    out of a "base" of snaps, deploy a new snap against it, then run a
+    set of integration tests.
+
+    '''
+
+    DEFAULT_BASE = [
+        {'name': 'rabbitmq', location='{snapstack}', tests=['rabbitmq.sh']},
+        {'name': 'database', location='{snapstack}', tests=['database.sh']},
+        {'name': 'keystone', location='{snapstack}', tests=['keystone.sh']},
+        {'name': 'nova', location='{snapstack}', tests=['nova.sh']},
+        {'name': 'neutron', location='{snapstack}', tests=['neutron.sh']},
+        {'name': 'glance', location='{snapstack}', tests=['glance.sh']},
+        {'name': 'nova-hypervisor', location='{snapstack}',
+         tests=['nova-hypervisor.sh']},
+        {'name': 'neutron-ext-net', location='{snapstack}',
+         tests=['neutron-ext-net.sh']},]
+
+    LOCATION_VARS = {
+        'snapstack': '../snapstack/scripts',  # TODO
+        'github': 'https://github.com/openstack-snaps-span-',
+        'local': 'tests',
+        'name': None  # Filled in by _run
+    }
+
+    def __init__(name, location='{local}', tests=None, base=None):
+        '''
+        @param string name: Name of the snap being tested.
+        @param string location: location of the test scripts for this snap.
+        @param list tests: List of scripts to execute in order.
+        @param list base: collection of snap tests to run to setup a "base"
+          environment.
+
+        '''
+        self._name = name
+        self._location = location
+        self._tests = tests
+        self._base = self._validate_base(base or self.DEFAULT_BASE)
+
+
+    def _validate_base(self, base):
+        # TODO
+        return base
+
+
+    def _path(self, location, test):
+        '''
+        Given a 'location' and a script name, return a path that
+        subprocess can use to execute the script.
+
+        If the location is a remote location, fetch the script to a
+        cache first. (TODO: or execute off of github?)
+
+        '''
+        if location.startswith('http') or location.startswith('ssh'):
+            # TODO
+            raise Exception('Github fetching not yet implemented')
+
+        return os.sep.join(location, test)
+
+
+    def _run(self, name, location, tests):
+        '''
+        Given a snap name, 'location' designator, and list of tests, run
+        the tests for that snap.
+
+        (This may be the main snap for this runner, or a snap in the 'base')
+
+        '''
+        tests = tests or []  # TODO: just fail if no tests?
+
+        location_vars = dict(self.LOCATION_VARS)  # Copy
+        location_vars['name'] = name
+        location = location.format(**location_vars)
+
+        for test in tests:
+            script = self._path(location, test)
+            if not subprocess.check_output(script):
+                raise TestFailure(
+                    'Failed to run test "{script}" for "{name}"'.format(
+                        script=script, name=name))
+
+
+    def run(self):
+        for spec in self._base:
+            try:
+                self._run(*spec)
+            except TestFailure as e:
+                # Transform TestFailure here into an InfraFailure, to
+                # help devs figure out just why the test failed.
+                if not self._name in self._base:
+                    raise InfraFailure('Snapstack setup failed: {}'.format(e))
+
+        if not self._name in self._base:
+            # Skip running tests separately for something that is
+            # already smoke tested in the base.
+            self._run(self._name, self._location, self._tests)
