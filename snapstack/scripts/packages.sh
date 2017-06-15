5e211142aca8f28aa92506d6047dfb92f3f46762
Pete Vander Giessen <pete.vandergiessen@canonical.com>
2017-06-15 16:50:33 -0400

    Maked packages.sh as executable.
    Cleaned up some extra stuff in it.

--- snapstack/scripts/packages.sh
+++ snapstack/scripts/packages.sh
@@ -9,9 +9,4 @@ sudo apt update
 DEBIAN_FRONTEND='noninteractive' sudo -E apt install --yes python3-openstackclient rabbitmq-server mysql-server \
     memcached libvirt-bin qemu-kvm apparmor-utils python-neutronclient openvswitch-switch
 
-SCRIPT=$(readlink -f $0)
-export BASE_DIR=$(dirname $SCRIPT)
-
-scripts="rabbitmq database keystone nova neutron glance nova-hypervisor neutron-ext-net"
-
 sudo snap install core
