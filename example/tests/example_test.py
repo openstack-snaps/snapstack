a628bc34e0dd2bddc6e5b885f0cca84f7186638c
Pete Vander Giessen <pete.vandergiessen@canonical.com>
2017-07-07 19:49:42 +0000

    Fixed up example test.

--- example/tests/example_test.py
+++ example/tests/example_test.py
@@ -5,7 +5,17 @@ from snapstack import Plan, Step, Setup, Cleanup
 
 class ExampleTest(unittest.TestCase):
 
-    def test(self):
+    def test_example(self):
+        '''
+        _test_example_
+
+        Basic test, which installs and tests and example
+        snap, after letting the Plan object build the default
+        snapstack.
+
+        Note: this will install snapstack on your system!
+
+        '''
         example = Step(
             snap='example',  # Name of the snap
             script_loc='./tests/',  # Parent location for tests and files
@@ -17,12 +27,14 @@ class ExampleTest(unittest.TestCase):
         plan = Plan(tests=[example])
         plan()
 
-    @unittest.skip("fnord")
     def test_custom_base(self):
+        '''
+        _test_custom_base_
 
-        # Reference code for adding and removing stuff from our Setup
-        # and Cleanup.
+        Reference code for adding and removing stuff from our Setup
+        and Cleanup.
 
+        '''
         setup = Setup()
         cleanup = Cleanup()
 
@@ -33,12 +45,12 @@ class ExampleTest(unittest.TestCase):
             tests=['example.sh'],
             files=['example.json'],
         )
-        setup.add_steps(example=example)
+        setup.add_steps(('example', example))
 
         # Remove steps.
         setup.remove_steps(
             'keystone', 'nova', 'neutron', 'glance', 'nova_hypervisor',
-            'neutron_exit_net')
+            'neutron_ext_net')
 
         # Run with our changes (this will download the config files,
         # but then install nothing but the example snap).
